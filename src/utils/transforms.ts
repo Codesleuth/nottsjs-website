b2e056900567e61029926a14eb300d299e05080e
David Wood <david.p.wood@gmail.com>
2020-04-04 13:57:35 +0100

    convert transforms

--- src/utils/transforms.ts
+++ src/utils/transforms.ts
@@ -1,10 +1,18 @@
+type Presenter = {
+  presenter: string
+  presenter_img: string
+  presenter_url: string
+  github_url: string
+  twitter_url: string
+}
+
 function transformPresenter ({
   presenter,
   presenter_img,
   presenter_url,
   github_url,
   twitter_url
-}, presenter_bio_html) {
+}: Presenter, presenter_bio_html: string) {
   return presenter ? ({
     name: presenter,
     bio: presenter_bio_html,
@@ -15,14 +23,14 @@ function transformPresenter ({
   }) : undefined
 }
 
-const suffixes = {
+const suffixes: Record<string, string> = {
   one: 'st',
   two: 'nd',
   few: 'rd',
   other: 'th',
 }
 
-function formatTime24(date) {
+function formatTime24(date: string) {
   return new Intl.DateTimeFormat('en-GB', {
     hour: 'numeric',
     minute: 'numeric',
@@ -30,7 +38,7 @@ function formatTime24(date) {
   }).format(new Date(date))
 }
 
-function formatTime12(date) {
+function formatTime12(date: string) {
   return new Intl.DateTimeFormat('en-GB', {
     hour: 'numeric',
     minute: 'numeric',
@@ -38,22 +46,34 @@ function formatTime12(date) {
   }).format(new Date(date))
 }
 
-function formatLongDate(date) {
+function formatLongDate(date: string) {
   const parts = new Intl.DateTimeFormat('en-GB', {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   }).formatToParts(new Date(date))
-  const weekday = parts.find(({ type }) => type === 'weekday').value
-  const year = parts.find(({ type }) => type === 'year').value
-  const month = parts.find(({ type }) => type === 'month').value
-  const day = parts.find(({ type }) => type === 'day').value
+  const weekday = parts.find(({ type }) => type === 'weekday')?.value
+  const year = parts.find(({ type }) => type === 'year')?.value
+  const month = parts.find(({ type }) => type === 'month')?.value
+  const day = Number(parts.find(({ type }) => type === 'day')?.value || 0)
   const pr = new Intl.PluralRules('en-GB', { type: 'ordinal' })
   return `${weekday}, ${month} ${day}${suffixes[pr.select(day)]}, ${year}`
 }
 
-export function transformEvent(eventNode) {
+type EventNode = {
+  html: string
+  frontmatter: {
+    path: string
+    title: string
+    start: string
+    end: string
+    meetup_url: string
+  } & Presenter
+  fields: any
+}
+
+export function transformEvent(eventNode: EventNode) {
   const {
     html,
     frontmatter: { path, title, start, end, meetup_url, ...otherFields },
